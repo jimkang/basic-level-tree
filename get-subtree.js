687e02a7b06eb0232876bf9ccc31bb1b2c9705dd
Jim Kang <jimkang@gmail.com>
2015-06-16 10:48:15 -0400

    Implemented getSubtree.

--- get-subtree.js
+++ get-subtree.js
@@ -1,7 +1,40 @@
-var callNextTick = require('call-next-tick');
+var queue = require('queue-async');
+var _ = require('lodash');
 
 function getSubtree(node, done) {
-  callNextTick(done);
+  var summary = {
+    // If there's performance issues, reconsider cloneDeep.
+    value: _.cloneDeep(node.value)
+  };
+
+  node.getChildren(addChildrenToSummary);
+
+  function addChildrenToSummary(error, children) {
+    var q;
+
+    if (error) {
+      done(error);
+    }
+    else {
+      q = queue(4);
+      children.forEach(queueRecursion);
+      q.awaitAll(passBackChildTrees);
+    }
+
+    function queueRecursion(child) {
+      q.defer(getSubtree, child);
+    }
+
+    function passBackChildTrees(error, childTrees) {
+      if (error) {
+        done(error);
+      }
+      else {
+        summary.children = childTrees;
+        done(error, summary);
+      }
+    }
+  }
 }
 
 module.exports = getSubtree;
